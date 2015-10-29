1111ea7a8a809fbf57eeddd9ef4f7fd8dec8dd8e
Joel Burton <joel@joelburton.com>
2015-10-29 00:51:33 -0700

    Add flask-script

---
+++ manage.py
@@ -0,0 +1,15 @@
+from flask.ext.script import Manager, Server
+from main import app
+
+manager = Manager(app)
+
+manager.add_command("server", Server())
+
+
+@manager.shell
+def make_shell_context():
+    return {'app': app}
+
+
+if __name__ == '__main__':
+    manager.run()
