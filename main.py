37972815d23f372f63b1a0bbe94e2336714e67be
Joel Burton <joel@joelburton.com>
2015-10-29 01:17:04 -0700

    Add tags.

--- main.py
+++ main.py
@@ -15,8 +15,52 @@ class User(db.Model):
     username = db.Column(db.String(255))
     password = db.Column(db.String(255))
 
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
+
     def __repr__(self):
         return "<User {}>".format(self.username)
+
+
+tags = db.Table("post_tags",
+                db.Column("post_id", db.Integer, db.ForeignKey("post.id")),
+                db.Column("tag_id", db.Integer, db.ForeignKey("tag.id"))
+                )
+
+class Post(db.Model):
+    """Blog post."""
+
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime())
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
+    comments = db.relationship("Comment", backref="post", lazy="dynamic")
+    tags = db.relationship("Tag", secondary=tags, backref=db.backref("posts", lazy="dynamic"))
+
+    def __repr__(self):
+        return "<Post {}>".format(self.title)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime())
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return "<Comment {}>".format(self.text[:15])
+
+
+class Tag(db.Model):
+    """Tags for blog posts."""
+
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+
+    def __repr__(self):
+        return "<Tag {}>".format(self.title)
 
 
 @app.route('/')
--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from flask.ext.script import Manager, Server
-from main import app, db, User
+from main import app, db, User, Post, Comment, Tag
 
 manager = Manager(app)
 
@@ -8,7 +8,7 @@ manager.add_command("server", Server())
 
 @manager.shell
 def make_shell_context():
-    return {'app': app, 'db': db, 'User': User}
+    return dict(app=app, db=db, User=User, Post=Post, Tag=Tag, Comment=Comment)
 
 
 if __name__ == '__main__':
