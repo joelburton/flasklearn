4acae6e4a04ffb9a09bc12e5a3f4f67b444ddfaa
Joel Burton <joel@joelburton.com>
2015-10-29 01:21:51 -0700

    Add alembic

--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
 from flask.ext.script import Manager, Server
+from flask.ext.migrate import Migrate, MigrateCommand
+
 from main import app, db, User, Post, Comment, Tag
+
+migrate = Migrate(app, db)
 
 manager = Manager(app)
 
 manager.add_command("server", Server())
+manager.add_command("db", MigrateCommand)
 
 
 @manager.shell
